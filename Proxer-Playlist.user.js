d55155a664a6d16b998c39d3377d74dfcbc79c48
Dravorle <dravorle@gmx.de>
2018-03-28 11:02:53 +0200

    Switched to using the Proxer-Player instead of a default one

--- Proxer-Playlist.user.js
+++ Proxer-Playlist.user.js
@@ -5,6 +5,7 @@
 // @include     https://proxer.me*
 // @supportURL  https://proxer.me/forum/283/384556
 // @updateURL   https://github.com/dravorle/ProxerPlaylist/raw/master/Proxer-Playlist.user.js
+// @version     1.6.4: Einbau des Proxer-Streams fertiggestellt
 // @version     1.6.3: Vorbereitungen um einige Funktionen auszulagern
 // @version     1.6.2: Vollbild-Funktion verschoben um sie API-Konform und damit funktionsfähig zu machen
 // @version     1.6.1: Kleiner CSS-Fehler, der dafür sorgte, dass Items komisch dargestellt werden
@@ -193,8 +194,7 @@ function StartPlay() {
     if( $("#Proxer-Playlist_Player").length === 0 ) {
         $("<div id='Proxer-Playlist_Player' data-current=''><div class='dim'></div><div class='playerWrapper'><video class='plyr'></video></div></div>").appendTo("body");
         
-        //Benötigt das SVG-Set von Proxer, kann derzeit aber nicht korrekt ausgeliefert werden, daher keine Buttons x_x
-        plyr.setup( { clickToPlay: false, iconUrl: "https://cdn.proxer.me/libs/psplayer/psplayer.svg" } );
+        plyr.setup( { clickToPlay: false, iconUrl: "https://proxer.me/images/misc/psplayer.svg" } );
         
         PlaylistVideo = $("#Proxer-Playlist_Player video")[0];
         PlaylistVideo.volume = Settings["volume"];
